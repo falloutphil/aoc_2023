193fab46de0459b8496b846b68251036938774fe
Phil Beadling <phil.beadling@opetek.io>
2023-12-25 14:40:22 +0000

    timestamp

--- common/result-utils.el
+++ common/result-utils.el
@@ -5,10 +5,13 @@
   (let ((buffer (get-buffer-create "*Advent of Code Results*")))  ; Create a new buffer
     (with-current-buffer buffer
       (erase-buffer)  ; Clear existing content
+      ;; Insert timestamp
+      (insert (format "Results as of: %s\n\n" (current-time-string)))
+      ;; Insert each message and corresponding result
       (cl-loop for result in results
                for message in messages
                do (insert (format "%s: %s\n" message result))))
     (switch-to-buffer-other-window buffer)))  ; Display the buffer
-
+                                        ;
 (provide 'result-utils)
 ;;; result-utils.el ends here
