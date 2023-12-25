7a1df49238c28231c8f94b597e346dd4164c5700
Phil Beadling <phil.beadling@opetek.io>
2023-12-25 22:07:00 +0000

    day 2 part 2

--- day-02/solution.el
+++ day-02/solution.el
@@ -49,10 +49,35 @@
       (when (is-game-possible game red-count green-count blue-count)
         (setq sum (+ sum (car game)))))))
 
+(defun minimum-cubes-for-game (game)
+  "We need to find the minimum number of red, green, and blue cubes for each game to be possible.
+   This means checking the maximum number of each colour shown at any point during the game."
+  (let ((red-max 0) (green-max 0) (blue-max 0))
+    (dolist (set (cdr game))    ; loop each game set
+      (dolist (pair set)        ; loop each colour in set as pair
+        (cond ((eq (car pair) 'red) (setq red-max (max red-max (cdr pair))))    ; update each max if larger candidate
+              ((eq (car pair) 'green) (setq green-max (max green-max (cdr pair))))
+              ((eq (car pair) 'blue) (setq blue-max (max blue-max (cdr pair)))))))
+    (list red-max green-max blue-max))) ; return max for each colour
+
+;; Calculate the power of a set of cubes
+;; which is just the multiplying colour together
+(defun power-of-set (red green blue)
+  (* red green blue))
+
 (defun day-02-part-01 (lines r g b)
   "Part 1"
   (let ((parsed-games (mapcar 'parse-game lines)))
     (process-games parsed-games r g b)))
+
+(defun day-02-part-02 (lines)
+  "Part 2"
+  (let ((parsed-games (mapcar 'parse-game lines))
+        (total-power 0))
+    (dolist (game parsed-games total-power)     ; loop each parsed game as game, return total-power
+      (let* ((min-cubes (minimum-cubes-for-game game))  ; return (r g b) list for the game
+             (power (apply 'power-of-set min-cubes)))   ; which is passed as params to power-of-set
+        (setq total-power (+ total-power power))))))    ; and added to the total
 
 (defvar day-02-test-data
   '("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
@@ -68,16 +93,20 @@
         (red-cubes 12)
         (green-cubes 13)
         (blue-cubes 14))
-    (should (= (process-games parsed-games red-cubes green-cubes blue-cubes)
-               8))))
+    ;; Part 1 Test
+    (should (= (process-games parsed-games red-cubes green-cubes blue-cubes) 8))
+    ;; Part 2 Test - Replace 2286 with the actual expected result for your test data
+    (should (= (day-02-part-02 day-02-test-data) 2286))))
 
 ;; Running the functions and displaying the results
 (let ((lines (read-lines input-file))
       (red-cubes 12)
       (green-cubes 13)
       (blue-cubes 14))
-  (display-results (list (day-02-part-01 lines red-cubes green-cubes blue-cubes))
-                   '("Part 01 - The sum of the IDs")))
+  (display-results (list (day-02-part-01 lines red-cubes green-cubes blue-cubes)
+                         (day-02-part-02 lines))
+                   '("Part 01 - The sum of the IDs"
+                     "Part 02 - The sum of the power of the minimum sets")))
 
 ;; Running the test
 (ert-run-tests-interactively "day-02-tests")
