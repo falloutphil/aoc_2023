d64d64fae0bebebd24bae7aefbd971c3b0a58472
Phil Beadling <phil.beadling@opetek.io>
2024-01-07 18:53:10 +0000

    Further templating

--- day-template/solution.el
+++ day-template/solution.el
@@ -25,23 +25,30 @@
             ))
   "Example schematic for day X.")
 
+(defun some-function (parsed-line)
+  1234)
+
+(defun some-other-function (parsed-line)
+  5678)
+
+
 (ert-deftest day-XX-tests ()
   (should (= (some-function day-XX-test-data) 1234))
-  (should (= (some-other-function day-XX-test-data) 1234)))
+  (should (= (some-other-function day-XX-test-data) 5678)))
 
 (defun day-XX-part-01 (lines)
   "Day X, Part 1."
-  (some-function (mapcar 'parse-card lines)))
+  (some-function (mapcar 'parse-input lines)))
 
 (defun day-XX-part-02 (lines)
   "Day X, Part 2."
-  (some-other-function (mapcar 'parse-card lines)))
+  (some-other-function (mapcar 'parse-input lines)))
 
 
-(let ((lines (read-lines day-XX-input-file)))
-  (display-results (list (day-XX-part-01 lines)
-                         (day-XX-part-02 lines))
-                   '("Part 01 - Some text to describe"
-                     "Part 02 - Some more text to describe")))
+;(let ((lines (read-lines day-XX-input-file)))
+;  (display-results (list (day-XX-part-01 lines)
+;                         (day-XX-part-02 lines))
+;                   '("Part 01 - Some text to describe"
+;                     "Part 02 - Some more text to describe")))
 
 (ert-run-tests-interactively "day-XX-tests")
